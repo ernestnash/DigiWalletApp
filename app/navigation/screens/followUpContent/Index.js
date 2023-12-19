0a3873e1dcadf1531b4708f4e0aa2d49697e2b0c
ernestnash <eernestnnashon@gmail.com>
2023-12-19 16:30:32 +0300

    Fixed: UIbugs

---
+++ app/navigation/screens/followUpContent/Index.js
@@ -0,0 +1,11 @@
+import TransactionDetails from "./TransactionDetails";
+import CardDetails from "./CardDetails";
+import NewCard from "./NewCard";
+
+
+
+export {
+    TransactionDetails,
+    CardDetails,
+    NewCard,
+}
